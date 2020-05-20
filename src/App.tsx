65b9f16db2fc3ff9ac2f019d07837c4acf54d813
Paulo Cardoso <rcjrpaulo@gmail.com>
2020-05-20 16:14:34 -0300

    starting project rn

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+const App: React.FC = () => (
+    <View>
+        <Text>Componente App</Text>
+    </View>
+);
+
+export default App;
