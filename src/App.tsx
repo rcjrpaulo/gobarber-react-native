32c03800e7a470c0563999cc2f2271c54db382c7
Paulo Cardoso <rcjrpaulo@gmail.com>
2020-05-20 17:14:15 -0300

    fix eslint

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,9 @@ import React from "react";
 import { View, Text } from "react-native";
 
 const App: React.FC = () => (
-    <View>
-        <Text>Componente App</Text>
-    </View>
+  <View>
+    <Text>Componente App</Text>
+  </View>
 );
 
 export default App;
