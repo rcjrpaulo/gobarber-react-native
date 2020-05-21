dc05606b9e658b4f0fa855c43156f392260e24b0
Paulo Cardoso <rcjrpaulo@gmail.com>
2020-05-21 17:38:13 -0300

    register is using api with node

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -14,6 +14,8 @@ import { Form } from "@unform/mobile";
 import { FormHandles } from "@unform/core";
 import * as Yup from "yup";
 
+import api from "../../services/api";
+
 import getValidationErrors from "../../utils/getValidationErrors";
 
 import Input from "../../components/Input";
@@ -56,14 +58,14 @@ const SignUp: React.FC = () => {
         abortEarly: false,
       });
 
-      // await api.post("/users", data);
+      await api.post("/users", data);
 
       Alert.alert(
-        "Cadastrado realizado!",
+        "Cadastrado realizado com sucesso!",
         "Você já pode fazer seu logon no GoBarber!"
       );
 
-      // history.push("/");
+      navigation.goBack();
     } catch (err) {
       if (err instanceof Yup.ValidationError) {
         const errors = getValidationErrors(err);
@@ -77,6 +79,7 @@ const SignUp: React.FC = () => {
         "Ocorreu um erro ao fazer cadastro, tente novamente",
         "Ocorreu um erro ao fazer login, cheque as credenciais."
       );
+      console.log(err);
     }
   }, []);
 
