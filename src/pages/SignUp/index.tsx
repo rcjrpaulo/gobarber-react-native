3c6be904e30b5b87c291ad5f21897e408f278f85
Paulo Cardoso <rcjrpaulo@gmail.com>
2020-05-21 18:36:17 -0300

    user is authenticating now

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -42,46 +42,49 @@ const SignUp: React.FC = () => {
   const passwordInputRef = useRef<TextInput>(null);
   const navigation = useNavigation();
 
-  const handleSignUp = useCallback(async (data: SignUpFormData) => {
-    try {
-      formRef.current?.setErrors({});
+  const handleSignUp = useCallback(
+    async (data: SignUpFormData) => {
+      try {
+        formRef.current?.setErrors({});
 
-      const schema = Yup.object().shape({
-        name: Yup.string().required("Nome obrigatório"),
-        email: Yup.string()
-          .required("E-mail obrigatório")
-          .email("Digite um E-mail válido"),
-        password: Yup.string().min(6, "No mínimo 6 digitos"),
-      });
+        const schema = Yup.object().shape({
+          name: Yup.string().required("Nome obrigatório"),
+          email: Yup.string()
+            .required("E-mail obrigatório")
+            .email("Digite um E-mail válido"),
+          password: Yup.string().min(6, "No mínimo 6 digitos"),
+        });
 
-      await schema.validate(data, {
-        abortEarly: false,
-      });
+        await schema.validate(data, {
+          abortEarly: false,
+        });
 
-      await api.post("/users", data);
+        await api.post("/users", data);
 
-      Alert.alert(
-        "Cadastrado realizado com sucesso!",
-        "Você já pode fazer seu logon no GoBarber!"
-      );
+        Alert.alert(
+          "Cadastrado realizado com sucesso!",
+          "Você já pode fazer seu logon no GoBarber!"
+        );
 
-      navigation.goBack();
-    } catch (err) {
-      if (err instanceof Yup.ValidationError) {
-        const errors = getValidationErrors(err);
+        navigation.goBack();
+      } catch (err) {
+        if (err instanceof Yup.ValidationError) {
+          const errors = getValidationErrors(err);
 
-        formRef.current?.setErrors(errors);
+          formRef.current?.setErrors(errors);
 
-        return;
+          return;
+        }
+
+        Alert.alert(
+          "Ocorreu um erro ao fazer cadastro, tente novamente",
+          "Ocorreu um erro ao fazer login, cheque as credenciais."
+        );
+        console.log(err);
       }
-
-      Alert.alert(
-        "Ocorreu um erro ao fazer cadastro, tente novamente",
-        "Ocorreu um erro ao fazer login, cheque as credenciais."
-      );
-      console.log(err);
-    }
-  }, []);
+    },
+    [navigation]
+  );
 
   return (
     <>
