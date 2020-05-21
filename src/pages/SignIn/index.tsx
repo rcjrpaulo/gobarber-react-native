2736ae7fd1ed454f4f992900b0ea582f58d35f62
Paulo Cardoso <rcjrpaulo@gmail.com>
2020-05-21 13:21:09 -0300

    starting styling SignIn page

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,8 +1,19 @@
 import React from "react";
-import { Text } from "react-native";
+import { Text, Image } from "react-native";
 
-import { Container } from "./styles";
+import { Container, Title } from "./styles";
 
-const SignIn: React.FC = () => <Text>SignIn</Text>;
+import logoImg from "../../assets/logo.png";
+
+const SignIn: React.FC = () => {
+  return (
+    <Container>
+      <Image source={logoImg} />
+      <Title>
+        Faça seu Logon
+      </Title>
+    </Container>
+  );
+};
 
 export default SignIn;
